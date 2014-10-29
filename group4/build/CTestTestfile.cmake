ae70c3b5b151760400303e20b79bf1097146e7c9
MTyk <nosgoth@gmx.net>
2014-10-29 16:41:12 +0200

    scripttest

--- group4/build/CTestTestfile.cmake
+++ group4/build/CTestTestfile.cmake
@@ -5,4 +5,6 @@
 # This file includes the relevent testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 SUBDIRS(gtest)
+SUBDIRS(liblms1xx)
+SUBDIRS(lms1xx)
 SUBDIRS(hippo2)
